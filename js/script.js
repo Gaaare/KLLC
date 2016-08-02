d5848eb6f450bdc123a0a0bd74bedc8afbc161a1
Garrett Stewart <satanthesith@gmail.com>
2016-08-02 04:09:26 -0700

    Fixed some stuff

--- js/script.js
+++ js/script.js
@@ -8,16 +8,7 @@ $(document).ready(function(){
 		
 	}) 
 	
-	$(".picWrap img").on("click",function(){
-		alert(1);
-		var img=$(this).attr("src");
-		console.log(img);
-		$(".largePic").removeClass(".hidden");
-		$(".largePic").html('<img src="'+img+'">');
-	})
-	$(".largePic").on("click", function(){
-		$(this).empty();
-	})
+	
 	var c4 = {explosive:"25", cloth: "5"};
 	   
 /* 	$(".submit").on("click",function(){
