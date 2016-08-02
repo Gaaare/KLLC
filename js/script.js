fe13d1ccfd438092e147642ae8087d2815029fd6
Garrett Stewart <satanthesith@gmail.com>
2016-08-02 04:02:15 -0700

    Changed some shit

--- js/script.js
+++ js/script.js
@@ -5,8 +5,19 @@ $(document).ready(function(){
 	})
 	$(".sectionBar").on("click",function(){
 		$(this).next(".drop").slideToggle();
-		alert(1);
+		
 	}) 
+	
+	$(".picWrap img").on("click",function(){
+		alert(1);
+		var img=$(this).attr("src");
+		console.log(img);
+		$(".largePic").removeClass(".hidden");
+		$(".largePic").html('<img src="'+img+'">');
+	})
+	$(".largePic").on("click", function(){
+		$(this).empty();
+	})
 	var c4 = {explosive:"25", cloth: "5"};
 	   
 /* 	$(".submit").on("click",function(){
